06896df892da623dee804113829e326a0674028e
zoraiz2000 <100046748+zoraiz2000@users.noreply.github.com>
2022-02-19 13:53:16 -0700

    Update stub.java

--- SCS - Hardware - v0.3 (2)/SCS - Hardware - v0.3/src/tests/stub.java
+++ SCS - Hardware - v0.3 (2)/SCS - Hardware - v0.3/src/tests/stub.java
@@ -4,5 +4,20 @@ import org.junit.Test;
 import java.util.*;
 
 public class stub implements ElectronicScaleObserver {
-
+	
+	void weightChanged(ElectronicScale scale, double weightInGrams)
+	{
+		
+	}
+	
+	void overload(ElectronicScale scale)
+	{
+		
+	}
+	
+	void outOfOverload(ElectronicScale scale);
+	{
+		
+	}
+  
 }
