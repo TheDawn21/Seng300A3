a4b01773e8a419ab1e7348ae925d429bfbdbee34
zoraiz2000 <100046748+zoraiz2000@users.noreply.github.com>
2022-02-19 14:13:49 -0700

    Update stub.java

--- SCS - Hardware - v0.3 (2)/SCS - Hardware - v0.3/src/tests/stub.java
+++ SCS - Hardware - v0.3 (2)/SCS - Hardware - v0.3/src/tests/stub.java
@@ -5,19 +5,25 @@ import java.util.*;
 
 public class stub implements ElectronicScaleObserver {
 	
+	private double weight;
+	private boolean overload;
+	private boolean outOfOverload;
+	
 	void weightChanged(ElectronicScale scale, double weightInGrams)
 	{
-		
+		weight = weightInGrams;
 	}
 	
 	void overload(ElectronicScale scale)
 	{
-		
+		overload = true;
+		outOfOverload = false;
 	}
 	
 	void outOfOverload(ElectronicScale scale);
 	{
-		
+		outOfOverload = true;
+		overload = false;
 	}
   
 }
