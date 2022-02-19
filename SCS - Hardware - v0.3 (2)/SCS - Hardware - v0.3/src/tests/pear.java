0858ad756e782128061bca60ca9dfd81e30ebbbb
Ethan Park <44657825+TheDawn21@users.noreply.github.com>
2022-02-19 15:38:31 -0700

    all tests for add method finished

---
+++ SCS - Hardware - v0.3 (2)/SCS - Hardware - v0.3/src/tests/pear.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.lsmr.selfcheckout.Item;
+
+public class pear extends Item{
+
+	pear()
+	{
+		super(2);
+	}
+}
--- SCS - Hardware - v0.3 (2)/SCS - Hardware - v0.3/src/tests/test.java
+++ SCS - Hardware - v0.3 (2)/SCS - Hardware - v0.3/src/tests/test.java
@@ -3,6 +3,8 @@ package tests;
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.*;
+
+import org.lsmr.selfcheckout.Item;
 import org.lsmr.selfcheckout.devices.*;
 
 public class test {
@@ -65,14 +67,30 @@ public class test {
 	@Test
 	public void testGetWeightLimit()
 	{
+		ElectronicScale a = new ElectronicScale(10,1);
+		assertTrue("Return incorrect value",10==a.getWeightLimit());
+	}
+	
+	@Test
+	public void testGetCurrentWeight() throws OverloadException
+	{
+		ElectronicScale a = new ElectronicScale(10,1);
+		a.endConfigurationPhase();
+		assertTrue("", 0==a.getCurrentWeight());
+	}
+	
+	@Test
+	public void testGetCurrentWeight2()
+	{
 		assertTrue("",false);
 	}
 	
 	@Test
-	public void testGetCurrentWeight()
+	public void testGetCurrentWeight3()
 	{
 		assertTrue("",false);
 	}
+	
 	
 	@Test
 	public void testGetSensitivity()
@@ -81,9 +99,82 @@ public class test {
 	}
 	
 	@Test
-	public void testAdd()
+	public void testAdd() throws OverloadException
 	{
-		assertTrue("",false);
+		ElectronicScale a = new ElectronicScale(10,1);
+		a.endConfigurationPhase();
+		Item pear;
+		pear b = new pear();
+		a.add(b);
+		int c = 2;
+		assertTrue("Weight different then expected",
+				c==(int)a.getCurrentWeight());
+	}
+	
+	@Test
+	public void testAdd2()
+	{
+		ElectronicScale a = new ElectronicScale(10,1);
+		Item pear;
+		pear b = new pear();
+		a.forceErrorPhase();
+		try
+		{
+			a.add(b);
+		}
+		catch(SimulationException ex)
+		{
+			assertTrue("Expected error thrown",true);
+			return;
+		}
+		assertTrue("Expected error not thrown",false);
+	}
+	
+	@Test 
+	public void testAdd3()
+	{
+		ElectronicScale a = new ElectronicScale(10,1);
+		pear b = new pear();
+		
+		try
+		{
+			a.add(b);
+		}
+		catch(SimulationException ex)
+		{
+			assertTrue("Expected error thrown", true);
+			return;
+		}
+		assertTrue("Expected error not thrown",false);
+	}
+	
+	@Test
+	public void testAdd4()
+	{
+		ElectronicScale a = new ElectronicScale(10,1);
+		a.endConfigurationPhase();
+		pear b = new pear();
+		a.add(b);
+		try 
+		{
+			a.add(b);
+		} 
+		catch (SimulationException ex) {
+			assertTrue("Item not added twice", true);
+			return;
+		}
+		assertTrue("Item added twice", false);
+	}
+
+	@Test
+	public void testAdd5()
+	{
+		ElectronicScale a = new ElectronicScale(1,1);
+		a.endConfigurationPhase();
+		pear b = new pear();
+		a.add(b);
+		assertTrue("Scale not overload when it should be", true);
+		
 	}
 	
 	@Test
